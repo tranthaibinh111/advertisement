30d3b035126bcc5056b70095759f94c856ea5b64
Binh Tran <tranthaibinh111@yahoo.com>
2019-03-05 02:18:05 +0700

    Change requirement by google company

--- advertisement-server/advertisement/urls.py
+++ advertisement-server/advertisement/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path(r'', views.home, name='home'),
     path(r'chinh-sach-van-chuyen', views.vanchuyen, name='vanchuyen'),
     path(r'chinh-sach-doi-tra', views.doitra, name='doitra'),
-    path(r'gio-hang', views.giohang, name='giohang'),
+    path(r'gio-hang/<int:sp_id>/', views.giohang, name='giohang'),
     path(r'thanh-toan', views.thanhtoan, name='thanhtoan'),
     path(r'hoan-tat', views.hoantat, name='hoantat'),
 ]
--- advertisement-server/config/settings.py
+++ advertisement-server/config/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DATABASE'),
+        'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASS'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('POSTGRES_HOST'),
         'PORT': env('POSTGRES_PORT'),
     }
