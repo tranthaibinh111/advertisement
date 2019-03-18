3623b31b145edc7fbabe695d7242de13108af667
Binh Tran <tranthaibinh111@yahoo.com>
2019-03-18 12:43:54 +0000

    Hoanh Anh, edit and update for CaoGung web

--- advertisement-server/advertisement/urls.py
+++ advertisement-server/advertisement/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path(r'', views.home, name='home'),
+    path(r'gio-hang/<int:sp_id>/', views.giohang, name='giohang'),
+    path(r'hoan-tat/<int:or_id>/', views.hoantat, name='hoantat'),
+    path(r'<str:sku>/<slug:slug>/', views.product, name='product'),
     path(r'chinh-sach-van-chuyen', views.vanchuyen, name='vanchuyen'),
     path(r'chinh-sach-doi-tra', views.doitra, name='doitra'),
-    path(r'gio-hang/<int:sp_id>/', views.giohang, name='giohang'),
-    path(r'thanh-toan', views.thanhtoan, name='thanhtoan'),
-    path(r'hoan-tat', views.hoantat, name='hoantat'),
+    path(r'dieu-khoan-dieu-kien-thanh-toan', views.dieukhoanthanhtoan, name='dieukhoanthanhtoan'),
 ]
